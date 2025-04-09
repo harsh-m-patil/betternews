064175b3d58354ff2ffaab6d00536a1d57b0485b
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2025-04-09 23:09:29 +0530

    Add auth with lucia

---
+++ server/context.ts
@@ -0,0 +1,11 @@
+import type { Env } from "hono";
+
+import type { User, Session } from "lucia";
+
+export interface Context extends Env {
+  Variables: {
+    user: User | null;
+
+    session: Session | null;
+  };
+}
