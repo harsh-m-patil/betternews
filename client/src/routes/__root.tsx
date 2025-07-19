78f6b8658011b3c4491d424c3a72301da388b683
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2025-07-19 11:24:51 +0530

    fix: remove tanstack-router-devtools from production build

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -3,11 +3,23 @@ import { Toaster } from "@/components/ui/sonner";
 import { type QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import React from "react";
 
 interface RouterContext {
   queryClient: QueryClient;
 }
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
 
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootComponent,
